39302492e81694778ec2f790df51e3075e5e4abb
ronielbackup1-coder <ronielbackup1@gmail.com>
2026-02-22 00:03:32 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,25 @@
-self.addEventListener("install", e => {
+const CACHE_NAME = "dom-music-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {});
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
